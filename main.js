1f6f105cca8c737548038686afa26e70548ddb3e
Natt Decodes <113068344+nattdecodes@users.noreply.github.com>
2023-10-14 14:31:08 -0400

    test please test help

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === "complete" && tab.active) {
-        alert("Dolphins are COOL!!!!!! ");
+document.addEventListener(`click`, e => {
+    const origin = e.target.closest(`a`);
+    
+    if (origin) {
+      console.clear();
+      console.log(`You clicked ${origin.href}`);
     }
-});
+  });
