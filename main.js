db7ddd46919c46b01bc830596df3e9007ffc1ebb
Natt Decodes <113068344+nattdecodes@users.noreply.github.com>
2023-10-14 14:23:12 -0400

    test

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-alert("Dolphins are COOL!!!!!! ");
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === "complete" && tab.active) {
+        alert("Dolphins are COOL!!!!!! ");
+    }
+});
