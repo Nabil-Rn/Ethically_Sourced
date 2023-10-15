e027213826b506e63d6e7b0cb456f25cf21ec235
Nabil-Rn <98413966+Nabil-Rn@users.noreply.github.com>
2023-10-15 00:42:36 -0400

    editing main.js

--- background.js
+++
@@ -1,9 +0,0 @@
-function openTab(){
-    
-    var newTab = browser.tabs.create({
-        url:'index.html',
-        active:true
-    })
-}
-
-browser.browserAction.onClicked.addListener(openTab);
--- main.js
+++ main.js
@@ -1,42 +1,46 @@
-document.addEventListener(`click`, e => {
-    const origin = e.target.closest(`a`);
-    
-    if (origin) {
-      console.clear();
-      console.log(`You clicked ${origin.href}`);
-    }
-  });
+const safeBrowsingApiKey = 'AIzaSyDy8guNmoR0XynEQRLojTUdtVP8XgO7gRY';
+const safeBrowsingURL = 'https://safebrowsing.googleapis.com/v4/threatMatches:find?key=' + safeBrowsingApiKey;
 
+// getting the url
+document.addEventListener('click', (e) => {
+    const urlToCheck = e.target.closest('a');
 
-const request = require('request');
-const body = {
-    threatInfo: {
-        threatTypes: ["SOCIAL_ENGINEERING", "MALWARE"],
-        platformTypes: ["ANY_PLATFORM"],
-        threatEntryTypes: ["URL"],
-        threatEntries: [{url: origin.href}]
-    }
-}
+    if (urlToCheck) {
+        console.clear();
+        console.log(`You clicked ${urlToCheck.href}`);
 
-const options = {
-    method: "POST",
-    url: "https://safebrowsing.googleapis.com/v4/threatMatches:find?key=AIzaSyDy8guNmoR0XynEQRLojTUdtVP8XgO7gRY",
-    json: body
-}
+      const url = urlToCheck.href;
 
-console.log(options);
-
-request(options,
-    function(err, res, data) {
-        console.log('data', data)
-        if (!err && res.statusCode == 200) {
-            console.log(data);
+      // Request body
+      const requestObject = {
+        client: {
+          clientVersion: '1.0.0'
+        },
+        threatInfo: {
+          threatTypes: ['MALWARE', 'SOCIAL_ENGINEERING', 'UNWANTED_SOFTWARE'],
+          platformTypes: ['ANY_PLATFORM'],
+          threatEntryTypes: ['URL'],
+          threatEntries: [{ url }]
         }
+      };
+
+      // Make the API request
+      fetch(safeBrowsingURL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestObject),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // Handle the response data here (contains threat information)
+          console.log(data);
+
+        })
+        .catch((error) => {
+          console.error('Error checking URL with Safe Browsing API:', error);
+        });
     }
-)
-/*
-POST https://safebrowsing.googleapis.com/v4/threatMatches:find?key=AIzaSyDy8guNmoR0XynEQRLojTUdtVP8XgO7gRY HTTP/1.1
-    Content-Type: application/json
-*/
 
-
+  });
